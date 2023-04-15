c2b1d4516b774be74b188970002ac98af2fd4d2b
tianwg <tianwg@users.noreply.github.com>
2023-04-14 22:14:54 -0400

    don't fail docker if no tag provided

--- entrypoint.sh
+++ entrypoint.sh
@@ -107,7 +107,7 @@ _git_tag() {
 
 _git_push() {
   [ -n "${INPUT_COMMIT_BRANCH}" ] && git push origin "${INPUT_COMMIT_BRANCH}" "${FORCE_OPT}"
-  [ -n "${INPUT_COMMIT_TAG}" ] && git push origin "${INPUT_COMMIT_TAG}" "${FORCE_OPT}"
+  [ -n "${INPUT_COMMIT_TAG}" ] && git push origin "${INPUT_COMMIT_TAG}" "${FORCE_OPT}" || true
 }
 
 
